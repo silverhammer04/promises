070f970488cd3d800f0ebb7f0cd870f90233b86f
Maxwell Seifert <silverhammer04@yahoo.com>
2020-07-02 11:08:29 -0600

    first commit

---
+++ promises.js
@@ -0,0 +1,43 @@
+
+let backend = (num) => {
+ const checker = new Promise((resolve, reject) => {
+    
+    if(num % 2 === 0 ) {
+        resolve('even')
+    } else {
+        reject('odd')
+    }
+ })   
+ return checker
+}
+const holder = (num) => {
+    backend(num).then((message) => {
+        console.log(`Resolved ${num} is ` + message)
+    }).catch((message) => {
+        console.log(`Rejected ${num} is ` + message)
+    });
+}
+holder(1)
+holder(2)
+
+let await = (num) => {
+    const checker = new Promise((resolve, reject) => {
+     setTimeout(() => { 
+         if(num % 2 === 0 ) {
+           resolve('even')
+       } else {
+           reject('odd')
+       }
+     }, 2000)  
+       
+    })   
+    checker.then((message) => {
+        console.log(`Resolved await ${num} is ` + message)
+    }).catch((message) => {
+        console.log(`Rejected await ${num} is ` + message)
+    })
+}   
+await(3);
+await(4);
+
+
